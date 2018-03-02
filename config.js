93369917e49a3b8a70014f9a7413fa30d4f3f126
Shaman Sharif <asif@localhost.localdomain>
2018-03-03 02:44:16 +0600

    Initial Commit

---
+++ config.js
@@ -0,0 +1,46 @@
+let colorsAndSizes = {
+	"2": {
+		size: 64,
+		color: "#F35956"
+	},
+	"4": {
+		size: 64,
+		color: "#F39556"
+	},
+	"8": {
+		size: 64,
+		color: "#9659A7"
+	},
+	"16": {
+		size: 64,
+		color: "#F1C500"
+	},
+	"32": {
+		size: 64,
+		color: "#574F21"
+	},
+	"64": {
+		size: 64,
+		color: "#04D299"
+	},
+	"128": {
+		size: 50,
+		color: "#0ABCD0"
+	},
+	"256": {
+		size: 50,
+		color: "#8D8BA8"
+	},
+	"512": {
+		size: 50,
+		color: "#099662"
+	},
+	"1024": {
+		size: 40,
+		color: "#D4DDAF"
+	},
+	"2048": {
+		size: 40,
+		color: "#ABCDEF"
+	}
+}
